8abe577880e1a146247a2cdaf937c122b20351cc
SofiaK <sonyabers401@gmail.com>
2016-01-25 14:16:34 +0200

    fixes at  profile page design

--- app/templates/profile-page/profile-page.js
+++ app/templates/profile-page/profile-page.js
@@ -1,7 +1,10 @@
 (function (){
     app.userData = app.sessionId ? JSON.parse(localStorage.getItem(app.apiUrl+ 'user')) : null;
-    app.userData.photo = app.userData.photo ? 'assets/' + app.userData.photo : 'assets/images/icon-profile.png';
     app.showEdit = false;
+
+    app.setUserPhoto = function(photo){
+        return photo ? 'assets/' + photo : 'assets/images/icon-profile.png';
+    };
 
     app.toggleProfileChanging = function(){
         return app.showEdit = !app.showEdit;
