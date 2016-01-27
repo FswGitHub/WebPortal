1b1379a341c6451ede5943fbb058f5a31a21bd74
SofiaK <sonyabers401@gmail.com>
2016-01-27 18:03:31 +0200

    add portfolio-utem bur

--- app/templates/portfolio-item/portfolio-item.js
+++ app/templates/portfolio-item/portfolio-item.js
@@ -1,3 +1,31 @@
 (function (){
+    app.chosenDate = formatDate(new Date());
+    app.selectedValue = 1;
 
+    app.openCalendar = function(){
+        var calendar = document.getElementById('calendar');
+        calendar.toggle();
+    };
+
+    app.openCalendarConf =  [
+        {name: 'fade-in-animation', timing: {delay: 150, duration: 200}},
+        {name: 'expand-animation', timing: {delay: 150, duration: 200}}
+    ];
+
+    app.closeCalendarConf = [
+        {name: 'fade-out-animation', timing: {duration: 200}}
+    ];
+
+    function formatDate(val){
+        var monthNames = ["Jan", "Feb", "Mar",
+            "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep",
+            "Oct", "Nov", "Dec"];
+        var date = new Date(val);
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+
+        return day+' '+monthNames[monthIndex]+' '+year;
+    }
 })();
